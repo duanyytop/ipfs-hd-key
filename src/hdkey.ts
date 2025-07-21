749f63e7f789b6c0f6ee945abac98265b22dbce4
Dylan Duan <duanyytop@gmail.com>
2025-07-21 15:03:14 +0800

    refactor: Update keychain import

--- src/hdkey.ts
+++ src/hdkey.ts
@@ -1,22 +1,46 @@
-import { hd } from "@ckb-lumos/lumos";
+import { hd, config, helpers, Script } from "@ckb-lumos/lumos";
+import { bytes } from "@ckb-lumos/lumos/codec";
 
-const { Keychain, Keystore, ExtendedPrivateKey, mnemonic } = hd;
+const { Keychain, Keystore, ExtendedPrivateKey, mnemonic, key } = hd;
 
 const CKB_PATH = `m/44'/309'/0'/0/0`;
 const IPFS_PATH = `m/1229997651'/0'/0`;
 const ORBITDB_PATH = `m/1768644974'/0'/0`;
 
 const testAcpAddress = async () => {
-  mnemonic.generateMnemonic()
+  mnemonic.generateMnemonic();
   const mnemonicStr = "solar snow traffic keep blade antique hold ginger clarify flock leaf mouse";
   const seed = mnemonic.mnemonicToSeedSync(mnemonicStr);
   const extendedPrivateKey = ExtendedPrivateKey.fromSeed(seed);
-  const root = extendedPrivateKey.serialize()
+  const root = extendedPrivateKey.serialize();
   console.log("root", root);
 
-  const keychain = Keychain.fromSeed(Buffer.from(root, "hex"));
+  const keychain = new Keychain(
+    Buffer.from(bytes.bytify(extendedPrivateKey.privateKey)),
+    Buffer.from(bytes.bytify(extendedPrivateKey.chainCode))
+  );
   const ckbChildKey = keychain.derivePath(CKB_PATH);
-  console.log(`CKB child key: ${`0x${Buffer.from(ckbChildKey.privateKey).toString("hex")}`}`);
+  const ckbKey = `0x${Buffer.from(ckbChildKey.privateKey).toString("hex")}`;
+  console.log(`CKB child key: ${ckbKey}`);
+
+  const args = key.privateKeyToBlake160(ckbKey);
+  const lumosConfig = config.TESTNET;
+  config.initializeConfig(lumosConfig);
+  const secp256k1Script: Script = {
+    codeHash: lumosConfig.SCRIPTS.SECP256K1_BLAKE160.CODE_HASH,
+    hashType: lumosConfig.SCRIPTS.SECP256K1_BLAKE160.HASH_TYPE,
+    args,
+  };
+  const ckbSecp256k1Address = helpers.encodeToAddress(secp256k1Script, {config: lumosConfig});
+  console.log(`CKBSecp256k1 address: ${ckbSecp256k1Address}`);
+
+  const acpScript: Script = {
+    codeHash: lumosConfig.SCRIPTS.ANYONE_CAN_PAY.CODE_HASH,
+    hashType: lumosConfig.SCRIPTS.ANYONE_CAN_PAY.HASH_TYPE,
+    args,
+  };
+  const ckbAcpAddress = helpers.encodeToAddress(acpScript, {config: lumosConfig});
+  console.log(`CKB ACP address: ${ckbAcpAddress}`);
 
   const ipfsChildKey = keychain.derivePath(IPFS_PATH);
   console.log(`IPFS child key: ${`0x${Buffer.from(ipfsChildKey.privateKey).toString("hex")}`}`);
@@ -54,17 +78,27 @@ const testAcpAddress = async () => {
   // Recover key from keystore
   const recover = Keystore.fromJson(keystoreJson);
   const recoveredExtendedPrivateKey = recover.extendedPrivateKey("password");
-  const recoveredRoot = recoveredExtendedPrivateKey.serialize();
-  console.log("Recovered root", recoveredRoot);
+  const masterKeyChain = new Keychain(
+    Buffer.from(bytes.bytify(recoveredExtendedPrivateKey.privateKey)),
+    Buffer.from(bytes.bytify(recoveredExtendedPrivateKey.chainCode))
+  );
 
-  const recoveredKeychain = Keychain.fromSeed(Buffer.from(recoveredRoot, "hex"));
-  const recoveredCkbChildKey = recoveredKeychain.derivePath(CKB_PATH).privateKey;
+  // Generate keychain from recovered root
+  // const recoveredRoot = recoveredExtendedPrivateKey.serialize();
+  // console.log("Recovered root", recoveredRoot);
+  // const rootKeyChain = new Keychain(
+  //   Buffer.from(bytes.bytify(recoveredRoot.slice(0, -64))),
+  //   Buffer.from(bytes.bytify(`0x${recoveredRoot.slice(-64)}`))
+  // );
+
+  // const recoveredKeychain = Keychain.fromSeed(Buffer.from(recoveredRoot, "hex"));
+  const recoveredCkbChildKey = masterKeyChain.derivePath(CKB_PATH).privateKey;
   console.log(`recovered CKB child key: ${`0x${Buffer.from(recoveredCkbChildKey).toString("hex")}`}`);
 
-  const recoveredIpfsChildKey = recoveredKeychain.derivePath(IPFS_PATH).privateKey;
+  const recoveredIpfsChildKey = masterKeyChain.derivePath(IPFS_PATH).privateKey;
   console.log(`recovered IPFS child key: ${`0x${Buffer.from(recoveredIpfsChildKey).toString("hex")}`}`);
 
-  const recoveredOrbitdbChildKey = recoveredKeychain.derivePath(ORBITDB_PATH).privateKey;
+  const recoveredOrbitdbChildKey = masterKeyChain.derivePath(ORBITDB_PATH).privateKey;
   console.log(`recovered OrbitDB child key: ${`0x${Buffer.from(recoveredOrbitdbChildKey).toString("hex")}`}`);
 }
 
